e5e0e4cd27866c43feb3b32509c021c04caa1285
Abhicoding <shinganeabhishek@gmail.com>
2018-03-13 10:47:45 +0530

    Minor change

--- Chapter 3/net-watcher.js
+++ Chapter 3/net-watcher.js
@@ -2,24 +2,18 @@
 const fs = require('fs')
 const net = require('net')
 const filename = process.argv[2]
-
 if (!filename) {
   throw Error('Error: No filename specified.')
 }
-
-const server = net.createServer(connection => {
-  // Use the connection object for data transfer
+net.createServer(connection => {
+  // Reporting.
   console.log('Subscriber connected.')
   connection.write(`Now watching "${filename}" for changes...\n`)
-
   // Watcher setup.
   const watcher = fs.watch(filename, () => connection.write(`File changed: ${new Date()}\n`))
-
   // Cleanup.
-  connection.on('close', () => {
+  /* connection.on('close', () => {
     console.log('Subscriber disconnected.')
     watcher.close()
-  })
-})
-
-server.listen('60300', () => console.log('Listening for subscribers...'))
+  }) */
+}).listen(60300, () => console.log('Listening for subscribers...'))
